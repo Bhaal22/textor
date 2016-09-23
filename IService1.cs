a17c10c440b01f445abb3b295aa0dde8fd088f4e
Your Name <jmuller@cnjmuller.ent.gsx.com>
2016-09-23 12:10:26 +0200

    service interface implementation

---
+++ IService1.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Textor
+{
+    public enum SortOption
+    {
+
+    }
+
+    [ServiceContract]
+    public interface ITextor
+    {
+
+        [OperationContract]
+        string Sort(string text, SortOption sortOption);
+
+        [OperationContract]
+        TextStatistics Statistics(string text);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class TextStatistics
+    {
+        private uint _hyphensCount = 0;
+        private uint _spacesCount = 0;
+        private uint _wordsCount = 0;
+
+        [DataMember]
+        public uint HyphensCount
+        {
+            get { return _hyphensCount; }
+            set { _hyphensCount = value; }
+        }
+
+        [DataMember]
+        public uint SpacesCount
+        {
+            get { return _spacesCount; }
+            set { _spacesCount = value; }
+        }
+
+        [DataMember]
+        public uint WordsCount
+        {
+            get { return _wordsCount; }
+            set { _wordsCount = value; }
+        }
+    }
+}

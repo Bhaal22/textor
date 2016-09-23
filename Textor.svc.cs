094e47771e1eb464d7f76f7b000120386db78738
Bhaal22 <muller.john@gmail.com>
2016-09-23 12:18:25 +0200

    renamed files

---
+++ Textor.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Textor
+{
+    public class Textor : ITextor
+    {
+        public string Sort(string text, SortOption sortOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TextStatistics Statistics(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+
+
+            return new TextStatistics();
+        }
+    }
+}
